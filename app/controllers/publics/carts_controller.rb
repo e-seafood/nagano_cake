93038e5807debd28d8b4687ce118b34d5de573fc
vagrant <vagrant@localhost.localdomain>
2020-10-03 07:42:28 +0000

    カート追加機能

--- app/controllers/publics/carts_controller.rb
+++ app/controllers/publics/carts_controller.rb
@@ -1,2 +1,14 @@
 class Publics::CartsController < ApplicationController
+
+def create
+  @cart = Cart.new(cart_params)
+  @cart.save
+  redirect_to items_path
+end
+
+private
+def cart_params
+  params.require(:cart).permit(:public_id, :item_id, :item_count)
+end
+
 end
