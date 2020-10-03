78f67ab2b346abb823032caf71e792f6073523e6
vagrant <vagrant@localhost.localdomain>
2020-10-03 05:17:54 +0000

    各コントローラー、ビュー作成

--- app/controllers/admins/publics_controller.rb
+++ app/controllers/admins/publics_controller.rb
@@ -1,2 +1,20 @@
 class Admins::PublicsController < ApplicationController
+	def index
+		@publics = Public.all
+	end
+
+	def show
+		@public = Public.find(params[:id])
+	end
+
+	def edit
+		@public = Public.find(params[:id])
+	end
+
+	def update
+		@public = Public.find(params[:id])
+		@public.save(public_params)
+		redirect_to admins_public_path(@public)
+	end
+
 end
