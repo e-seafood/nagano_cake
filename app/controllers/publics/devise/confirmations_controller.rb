b4e98b8ed389534b9362c0d8ca199e3935da678d
青島優 <aoshima.yu.0402@gmail.com>
2020-10-03 15:22:03 +0900

    second commit

--- app/controllers/publics/devise/confirmations_controller.rb
+++ app/controllers/publics/devise/confirmations_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Publics::ConfirmationsController < Devise::ConfirmationsController
+class Publics::Devise::ConfirmationsController < Devise::ConfirmationsController
   # GET /resource/confirmation/new
   # def new
   #   super
--- app/controllers/publics/publics_controller.rb
+++ app/controllers/publics/publics_controller.rb
@@ -8,25 +8,18 @@ class Publics::PublicsController < ApplicationController
 	end
 
 	def withdraw
-    @public = current_customer
-    @public.update(is_valid: true)
-
-    reset_session
-    flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
-    redirect_to root_path
 	end
 
 	def edit
-    @public = current_customer
+    @public = current_public
 	end
 
 	def update
-    @public = current_customer
+    @public = current_public
 		if @public.update(public_params)
-      flash[:success] = "登録情報を変更しました"
-      redirect_to customers_path
+      redirect_to publics_path
     else
-      render :edit and return
+      render :edit 
     end
 	end
 
