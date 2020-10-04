6ddfd0db0a81f44c4a5a740771974c2b4b97284a
vagrant <vagrant@localhost.localdomain>
2020-10-04 08:31:50 +0000

    carts_create

--- app/controllers/publics/carts_controller.rb
+++ app/controllers/publics/carts_controller.rb
@@ -1,2 +1,24 @@
 class Publics::CartsController < ApplicationController
+
+  def index
+    @carts = Cart.all
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+    @cart.public.id = current_public.id
+    @cart_item = Cart.find_by(item_id: @cart.item_id)
+    if @cart_item.present?
+      @cart.item_count += @cart_item.item_count
+      @cart_item.destroy
+    end
+    @cart.save
+    redirect_to carts_path
+  end
+
+  private
+  def cart_params
+    params.require(:cart).permit(:public_id, :item_id, :item_count)
+  end
+
 end
