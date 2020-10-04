876bf00bba87b08351bad136c5f4681f61f2519d
vagrant <vagrant@localhost.localdomain>
2020-10-04 02:14:46 +0000

    コンフリクト解消

--- app/controllers/publics/carts_controller.rb
+++ app/controllers/publics/carts_controller.rb
@@ -1,14 +1,2 @@
 class Publics::CartsController < ApplicationController
-
-def create
-  @cart = Cart.new(item_params)
-  @cart.save
-  redirect_to carts_path
-end
-
-  private
-  def cart_params
-    params.require(:cart).permit(:public_id, :item_id, :item_count)
-  end
-
 end
