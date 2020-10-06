d2095f10fe14e8aa018b0a451877b05656498a0f
saito743 <yuka.saito21@gmail.com>
2020-10-06 05:20:38 +0000

    商品ごとの小計計算

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
-
+#消費税の計算
   def tax(price)
     ((price*1.1).round(2)).ceil
   end
-
+#注文商品ごとの小計
+  def amount(price,item_count)
+  	(price*item_count)
+  end
+#送料
   def post_age
   	800.to_i
   end
