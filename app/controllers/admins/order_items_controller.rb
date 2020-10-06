9f242310348adb7a65ce032becbbbe2da0633e9f
saito743 <yuka.saito21@gmail.com>
2020-10-06 13:06:37 +0000

    製作ステータス変更

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -1,2 +1,11 @@
 class Admins::OrderItemsController < ApplicationController
+	def update
+		@order = Order.find(params[:id])
+		order_item = OrderItem.find(params[:id])
+		order_item.update(order_item_params)
+	end
+private
+	def order_item_params
+		params.require(:order_item).permit(:item_count,:making_status,:tax_icluded_price)
+	end
 end
--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -13,6 +13,7 @@ class Admins::OrdersController < ApplicationController
 	def update
 		@order = Order.find(params[:id])
   		@order.update(order_params)
+  		order_item.update(order_item_params)
   		redirect_to request.referer
 	end
 
@@ -21,6 +22,10 @@ class Admins::OrdersController < ApplicationController
 private
 	def order_params
 		params.require(:order).permit(:status,:postcode,:address,:name,:payment,:postage,:amount)
+	end
+
+	def order_item_params
+		params.require(:order_item).permit(:item_count,:status,:tax_icluded_price)
 	end
 
 end
