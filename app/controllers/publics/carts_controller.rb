06b1e2adaa7be208cfee918be8ac67c9530a2286
vagrant <vagrant@localhost.localdomain>
2020-10-08 03:08:09 +0000

    検索機能

--- app/controllers/publics/carts_controller.rb
+++ app/controllers/publics/carts_controller.rb
@@ -1,7 +1,7 @@
 class Publics::CartsController < ApplicationController
 
   def index
-    @carts = Cart.where(public_id: current_public.id).order(item_id: "ASC")
+    @carts = Cart.where(public_id: current_public.id).order(:item_id)
   end
 
   def create
